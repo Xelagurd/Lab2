1636b52e84b876cf72bef85865ba9b2b330fd55f
DrAlexxD <alexander.drugakov@mail.ru>
2019-10-18 21:05:31 +0300

    Scripted auto-commit on change (2019-10-18 21:05:31) by gitwatch.sh

--- src/main/java/lab2/ReduceSideJoinJob.java
+++ src/main/java/lab2/ReduceSideJoinJob.java
@@ -1,7 +1,11 @@
 package lab2;
 
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class ReduceSideJoinJob {
     public static void main(String[] args) throws Exception {
@@ -12,6 +16,9 @@ public class ReduceSideJoinJob {
         Job job = Job.getInstance();
         job.setJarByClass(ReduceSideJoinJob.class);
         job.setJobName("Reduce Side Join");
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightTableMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportTableMapper.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
 
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
