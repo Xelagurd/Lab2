9ba4aeb7d386959074843b3ecf36313b456bb95b
DrAlexxD <alexander.drugakov@mail.ru>
2019-10-18 21:07:47 +0300

    Scripted auto-commit on change (2019-10-18 21:07:47) by gitwatch.sh

--- src/main/java/lab2/AirportIDOnlyPartitioner.java
+++ src/main/java/lab2/AirportIDOnlyPartitioner.java
@@ -1,4 +1,10 @@
 package lab2;
 
-public class AirportIDOnlyPartitioner {
+import org.apache.hadoop.io.Text;
+
+public class AirportIDOnlyPartitioner extends Partitioner<ComponentKey, Text> {
+    /*u. Разрабатываем Partitioner, который учитывает только код аэропорта */
+    public AirportIDOnlyPartitioner() {
+    }
+
 }
