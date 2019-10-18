2bf9292067dcc6ebaf80cea3172bf579f1d4d114
DrAlexxD <alexander.drugakov@mail.ru>
2019-10-18 21:04:41 +0300

    Scripted auto-commit on change (2019-10-18 21:04:40) by gitwatch.sh

--- src/main/java/lab2/FlightTableMapper.java
+++ src/main/java/lab2/FlightTableMapper.java
@@ -13,6 +13,8 @@ public class FlightTableMapper extends Mapper<LongWritable, Text, ComponentKey, 
     для списка рейсов в качестве value эта функция отправляет время задержки (в виде строки)*/
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-
+        FlightTableWritable obj = new FlightTableWritable(value.toString());
+        Pair<String, String> pair = obj.getDelayPair();
+        context.write(new ComponentKey(pair.getKey(), 1), new Text(pair.getValue()));
     }
 }
