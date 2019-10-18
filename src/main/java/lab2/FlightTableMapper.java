5930f4bda2c827c0d8eae734d1baadd80bba7e4d
DrAlexxD <alexander.drugakov@mail.ru>
2019-10-18 21:04:21 +0300

    Scripted auto-commit on change (2019-10-18 21:04:20) by gitwatch.sh

--- src/main/java/lab2/FlightTableMapper.java
+++ src/main/java/lab2/FlightTableMapper.java
@@ -4,7 +4,15 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
+import javafx.util.Pair;
+
 public class FlightTableMapper extends Mapper<LongWritable, Text, ComponentKey, Text> {
     /*в.разрабатываем map функцию для каждого из набора данных, которая генерирует  WritableComparable ключа и Writable данных
     для списка рейсов в качестве value эта функция отправляет время задержки (в виде строки)*/
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+    }
 }
