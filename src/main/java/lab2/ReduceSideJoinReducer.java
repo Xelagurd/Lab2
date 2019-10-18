903e351617be485ea93c66ed3783031033b719b4
DrAlexxD <alexander.drugakov@mail.ru>
2019-10-18 21:12:57 +0300

    Scripted auto-commit on change (2019-10-18 21:12:56) by gitwatch.sh

--- src/main/java/lab2/ReduceSideJoinReducer.java
+++ src/main/java/lab2/ReduceSideJoinReducer.java
@@ -1,4 +1,7 @@
 package lab2;
 
-public class ReduceSideJoinReducer {
+public class ReduceSideJoinReducer extends Reducer<ComponentKey, Text, Text, Text> {
+    /*е. Разрабатываем reduce функцию, которая берет первую строку, извлекает из нее
+     имя аэропорта, далее рассчитывает из последующих строк среднее минимальное и
+      максимальное время задержки и печатает результат. */
 }
