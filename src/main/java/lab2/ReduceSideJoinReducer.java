4a639adb990e2a6575e77ff4eb9bade3df9659e2
DrAlexxD <alexander.drugakov@mail.ru>
2019-10-18 21:13:26 +0300

    Scripted auto-commit on change (2019-10-18 21:13:26) by gitwatch.sh

--- src/main/java/lab2/ReduceSideJoinReducer.java
+++ src/main/java/lab2/ReduceSideJoinReducer.java
@@ -1,7 +1,17 @@
 package lab2;
 
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
 public class ReduceSideJoinReducer extends Reducer<ComponentKey, Text, Text, Text> {
     /*е. Разрабатываем reduce функцию, которая берет первую строку, извлекает из нее
      имя аэропорта, далее рассчитывает из последующих строк среднее минимальное и
       максимальное время задержки и печатает результат. */
+    @Override
+    protected void reduce(ComponentKey key, Iterable<Text> values, Context context) throws
+            IOException, InterruptedException {
+
+    }
 }
