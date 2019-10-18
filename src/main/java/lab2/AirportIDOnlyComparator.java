cb9acc0a254947be3f857a0788bc81dd8a559d3d
DrAlexxD <alexander.drugakov@mail.ru>
2019-10-18 21:10:16 +0300

    Scripted auto-commit on change (2019-10-18 21:10:15) by gitwatch.sh

--- src/main/java/lab2/AirportIDOnlyComparator.java
+++ src/main/java/lab2/AirportIDOnlyComparator.java
@@ -4,5 +4,10 @@ import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class AirportIDOnlyComparator extends WritableComparator {
+    /*д.  Разрабатываем GroupingComparatorClass, который учитывает только  код аэропорта */
+    public AirportIDOnlyComparator() {
+        super(ComponentKey.class, true);
+    }
 
+    
 }
