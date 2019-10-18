18d52ff33008e14edaa8003776b7a36907ae4169
DrAlexxD <alexander.drugakov@mail.ru>
2019-10-18 21:01:35 +0300

    Scripted auto-commit on change (2019-10-18 21:01:35) by gitwatch.sh

--- src/main/java/lab2/AiroportTableMapper.java
+++ src/main/java/lab2/AiroportTableMapper.java
@@ -1,4 +1,14 @@
 package lab2;
 
-public class AiroportTableMapper {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+import javafx.util.Pair;
+
+public class AirportTableMapper extends Mapper<LongWritable, Text, ComponentKey, Text> {
+    /*в.разрабатываем map функцию для каждого из набора данных, которая генерирует  WritableComparable ключа и Writable данных
+    для списка аэропортов эта функция в качестве value отправляет имя аэропорта.*/
 }
