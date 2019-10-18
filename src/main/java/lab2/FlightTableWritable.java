df65aa33c1d3a10c722fbb422fb71fe9a5fe7086
DrAlexxD <alexander.drugakov@mail.ru>
2019-10-18 20:48:21 +0300

    Scripted auto-commit on change (2019-10-18 20:48:20) by gitwatch.sh

--- src/main/java/lab2/FlightTableWritable.java
+++ src/main/java/lab2/FlightTableWritable.java
@@ -7,9 +7,11 @@ public class FlightTableWritable {
     private Pair<String, String> pair;
 
     public static final int TOTAL = 18;
-    
+    public static final int AIRPORT_ID_INDEX = 14;
+    public static final int DELAY_INDEX = 17;
+
     public FlightTableWritable(String table) {
-        if (table.length() > 1){
+        if (table.length() > 1 && !table.contains("YEAR")){
             String[] parts = table.split(",");
         }
     }
