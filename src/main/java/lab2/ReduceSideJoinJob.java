b9a26a1d039869f775f11a2408f756a72eeab1e2
DrAlexxD <alexander.drugakov@mail.ru>
2019-10-18 20:51:54 +0300

    Scripted auto-commit on change (2019-10-18 20:51:54) by gitwatch.sh

--- src/main/java/lab2/ReduceSideJoinJob.java
+++ src/main/java/lab2/ReduceSideJoinJob.java
@@ -1,4 +1,10 @@
 package lab2;
 
 public class ReduceSideJoinJob {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 3) {
+            System.err.println("Usage: ReduceSideJoinJob <input path Flights> <input path Airports> <output path>");
+            System.exit(-1);
+        }
+    }
 }
