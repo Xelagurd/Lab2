3b79c3e4460cd5f22d75a6172e229cbe2ca50e79
DrAlexxD <alexander.drugakov@mail.ru>
2019-10-18 20:37:14 +0300

    Scripted auto-commit on change (2019-10-18 20:37:14) by gitwatch.sh

---
+++ src/main/java/lab2/AirportTableWritable.java
@@ -0,0 +1,10 @@
+package lab2;
+
+import javafx.util.Pair;
+
+public class AirportTableWritable {
+    /*а.Разрабатываем Writable для каждого из входных наборов данных, который может читать данные из csv. */
+    private Pair<String, String> pair;
+
+
+}
