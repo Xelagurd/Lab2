4a82fec97f9b97d3a447360fbdfe44cba8ba7b25
DrAlexxD <alexander.drugakov@mail.ru>
2019-10-18 20:53:14 +0300

    Scripted auto-commit on change (2019-10-18 20:53:14) by gitwatch.sh

--- src/main/java/lab2/ReduceSideJoinJob.java
+++ src/main/java/lab2/ReduceSideJoinJob.java
@@ -1,10 +1,17 @@
 package lab2;
 
+import org.apache.hadoop.mapreduce.Job;
+
 public class ReduceSideJoinJob {
     public static void main(String[] args) throws Exception {
         if (args.length != 3) {
             System.err.println("Usage: ReduceSideJoinJob <input path Flights> <input path Airports> <output path>");
             System.exit(-1);
         }
+        Job job = Job.getInstance();
+        job.setJarByClass(ReduceSideJoinJob.class);
+        job.setJobName("Reduce Side Join");
+
+        
     }
 }
