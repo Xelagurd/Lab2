47793135ad44ea734fe3543e8b2db480d9e8e036
DrAlexxD <alexander.drugakov@mail.ru>
2019-10-18 20:42:55 +0300

    Scripted auto-commit on change (2019-10-18 20:42:55) by gitwatch.sh

--- src/main/java/lab2/AirportTableWritable.java
+++ src/main/java/lab2/AirportTableWritable.java
@@ -6,7 +6,14 @@ public class AirportTableWritable {
     /*а.Разрабатываем Writable для каждого из входных наборов данных, который может читать данные из csv. */
     private Pair<String, String> pair;
 
-    public AirportTableWritable(String d){
+    public AirportTableWritable(String table){
+        int index;
+        String id, name;
 
+        if (!table.contains("Description")){
+            index = table.indexOf(",");
+            name = table.substring(index + 2, table.length() - 1);
+            
+        }
     }
 }
