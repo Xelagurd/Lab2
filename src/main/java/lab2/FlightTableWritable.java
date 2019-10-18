f7dcd646e74c86f3dfa63172f15e1c367a83f939
DrAlexxD <alexander.drugakov@mail.ru>
2019-10-18 20:48:01 +0300

    Scripted auto-commit on change (2019-10-18 20:48:01) by gitwatch.sh

--- src/main/java/lab2/FlightTableWritable.java
+++ src/main/java/lab2/FlightTableWritable.java
@@ -6,7 +6,11 @@ public class FlightTableWritable {
     /*а.Разрабатываем Writable для каждого из входных наборов данных, который может читать данные из csv. */
     private Pair<String, String> pair;
 
+    public static final int TOTAL = 18;
+    
     public FlightTableWritable(String table) {
-
+        if (table.length() > 1){
+            String[] parts = table.split(",");
+        }
     }
 }
